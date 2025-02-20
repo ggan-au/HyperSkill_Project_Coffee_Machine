0c420a30d01ced0e8a7314d9e4d8197a0467d667
ai1002hash <glenganderton@proton.me>
2025-02-20 17:54:43 +1100

    Passed Tests - Stage 4/6 - Complete

--- Coffee Machine (Python)/task/machine/coffee_machine.py
+++ Coffee Machine (Python)/task/machine/coffee_machine.py
@@ -1,23 +1,71 @@
-WATER_PER_CUP = 200
-MILK_PER_CUP = 50
-BEANS_GRAM_PER_CUP = 15
+import textwrap
 
-def coffee_cups_remaining(water, milk, coffee):
-    water_cups = water // WATER_PER_CUP
-    milk_cups = milk // MILK_PER_CUP
-    coffee_beans = coffee // BEANS_GRAM_PER_CUP
-    return min(water_cups, milk_cups, coffee_beans)
+class CoffeeMachine:
+    def __init__(self, water, milk, beans, cups, money):
+        self.water = water
+        self.milk = milk
+        self.beans = beans
+        self.cups = cups
+        self.money = money
 
-water_remaining = int(input("Write how many ml of water the coffee machine has: "))
-milk_remaining = int(input("How many ml of milk the coffee machine has: "))
-coffee_beans_remaining = int(input("Write how many grams of coffee beans the coffee machine has: "))
+    def get_status(self):
+        status = textwrap.dedent(f"""
+                 The coffee machine has:
+                 {self.water} ml of water
+                 {self.milk} ml of milk
+                 {self.beans} g of coffee beans
+                 {self.cups} disposable cups
+                 ${self.money} of money
+                 """)
+        return status
 
-total_remaining_coffee_cups = coffee_cups_remaining(water_remaining, milk_remaining, coffee_beans_remaining)
-coffee_cups_requested = int(input("Write how many cups of coffee you will need: "))
+    def reduce_supplies(self, water=0, milk=0, beans=0, cups=1):
+        self.water -= water
+        self.milk -= milk
+        self.beans -= beans
+        self.cups -= cups
 
-if total_remaining_coffee_cups == coffee_cups_requested:
-    print("Yes, I can make that amount of coffee")
-elif total_remaining_coffee_cups > coffee_cups_requested:
-    print(f"Yes, I can make that amount of coffee (and even {total_remaining_coffee_cups - coffee_cups_requested} more than that)")
-else:
-    print(f"No, I can make only {total_remaining_coffee_cups} cups of coffee")
+    def increase_supplies(self, water=0, milk=0, beans=0, cups=0):
+        self.water += water
+        self.milk += milk
+        self.beans += beans
+        self.cups += cups
+
+    def fill(self):
+        water = int(input("Write how many ml of water you want to add: "))
+        milk = int(input("Write how many ml of milk you want to add: "))
+        beans = int(input("Write how many grams of coffee beans you want to add: "))
+        cups = int(input("Write how many disposable cups you want to add: "))
+        self.increase_supplies(water, milk, beans, cups)
+
+    def take(self):
+        money_take = self.money
+        self.money = 0
+        print(f"I gave you ${money_take}")
+
+    def buy_coffee(self):
+        menu_option = int(input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: "))
+        if menu_option == 1:
+            self.reduce_supplies(water=250, beans=16)
+            self.money += 4
+        if menu_option == 2:
+            self.reduce_supplies(water=350, milk=75, beans=20)
+            self.money += 7
+        if menu_option == 3:
+            self.reduce_supplies(water=200, milk=100, beans=12)
+            self.money += 6
+
+
+coffee_machine = CoffeeMachine(400, 540, 120, 9, 550)
+
+print(coffee_machine.get_status())
+action = input("Write action (buy, fill, take):")
+if action == "buy":
+    coffee_machine.buy_coffee()
+    print(coffee_machine.get_status())
+if action == "fill":
+    coffee_machine.fill()
+    print(coffee_machine.get_status())
+if action == "take":
+    coffee_machine.take()
+    print(coffee_machine.get_status())
