f137d0e1a67410cb0cbcb4517137244fd9335ee2
ai1002hash <glenganderton@proton.me>
2025-02-21 16:06:44 +1100

    Passed Tests - Stage 5/6 - Complete

--- Coffee Machine (Python)/task/machine/coffee_machine.py
+++ Coffee Machine (Python)/task/machine/coffee_machine.py
@@ -19,11 +19,27 @@ class CoffeeMachine:
                  """)
         return status
 
+    def check_supply_level(self, water, milk, beans, cups):
+        if self.water < water:
+            raise Exception("water")
+        if self.milk < milk:
+            raise Exception("milk")
+        if self.beans < beans:
+            raise Exception("beans")
+        if self.cups < cups:
+            raise Exception("cups")
+
     def reduce_supplies(self, water=0, milk=0, beans=0, cups=1):
-        self.water -= water
-        self.milk -= milk
-        self.beans -= beans
-        self.cups -= cups
+        try:
+            self.check_supply_level(water, milk, beans, cups)
+        except Exception as e:
+            raise ValueError(f"Sorry, not enough {e}!")
+        else:
+            print("I have enough resources, making you a coffee!")
+            self.water -= water
+            self.milk -= milk
+            self.beans -= beans
+            self.cups -= cups
 
     def increase_supplies(self, water=0, milk=0, beans=0, cups=0):
         self.water += water
@@ -44,28 +60,42 @@ class CoffeeMachine:
         print(f"I gave you ${money_take}")
 
     def buy_coffee(self):
-        menu_option = int(input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: "))
-        if menu_option == 1:
+        menu_option = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ")
+        if menu_option == "1":
             self.reduce_supplies(water=250, beans=16)
             self.money += 4
-        if menu_option == 2:
+        if menu_option == "2":
             self.reduce_supplies(water=350, milk=75, beans=20)
             self.money += 7
-        if menu_option == 3:
+        if menu_option == "3":
             self.reduce_supplies(water=200, milk=100, beans=12)
             self.money += 6
+        if menu_option == "back":
+            return
 
+def main_menu(machine):
+    display_menu = True
+
+    while display_menu:
+        action = input("Write action (buy, fill, take, remaining, exit):")
+        if action == "buy":
+            try:
+                machine.buy_coffee()
+            except Exception as e:
+                print(e)
+        if action == "fill":
+            machine.fill()
+        if action == "take":
+            machine.take()
+        if action == "remaining":
+            print(machine.get_status())
+        if action == "exit":
+            display_menu = False
 
 coffee_machine = CoffeeMachine(400, 540, 120, 9, 550)
+main_menu(coffee_machine)
 
-print(coffee_machine.get_status())
-action = input("Write action (buy, fill, take):")
-if action == "buy":
-    coffee_machine.buy_coffee()
-    print(coffee_machine.get_status())
-if action == "fill":
-    coffee_machine.fill()
-    print(coffee_machine.get_status())
-if action == "take":
-    coffee_machine.take()
-    print(coffee_machine.get_status())
+
+
+
+
